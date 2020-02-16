a560a894aaa4ef78cdf8c704afd006fdcc2b187e
Alexis Sanders <asanders@merkleinc.com>
2020-02-16 14:22:55 -0500

    set up

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # starting example from https://github.com/timothyrjames/cs1520/blob/master/week04/gae/project2/main.py
 
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return redirect("/s/index.html", code=302)
+    return render_template('/index.html')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8050, debug=True) # updated port, so that when it runs locally, it runs on 8050
+        app.run(host='127.0.0.1', port=8040, debug=True) # updated port, so that when it runs locally, it runs on 8040
