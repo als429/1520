0b81f732b812ad68ea784c8cd63de35bb68f1be9
alexis <asanders>
2020-03-01 14:31:11 -0500

    testing login stuff

--- main.py
+++ main.py
@@ -35,30 +35,6 @@ def eatlist():
 # backend sign in functionality
 @app.route('/authtoken', methods=['POST'])
 def authtoken():
-	# (Receive token by HTTPS POST)
-	# ...
-
-	try:
-		# Specify the CLIENT_ID of the app that accesses the backend:
-		idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
-
-		# Or, if multiple clients access the backend server:
-		# idinfo = id_token.verify_oauth2_token(token, requests.Request())
-		# if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
-		#     raise ValueError('Could not verify audience.')
-
-		if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-			raise ValueError('Wrong issuer.')
-
-		# If auth request is from a G Suite domain:
-		# if idinfo['hd'] != GSUITE_DOMAIN_NAME:
-		#     raise ValueError('Wrong hosted domain.')
-
-		# ID token is valid. Get the user's Google Account ID from the decoded token.
-		userid = idinfo['sub']
-	except ValueError:
-		# Invalid token
-		pass
 	log('Token: ' + request.form.get('token'))
     d = {
         'message': 'Auth Token received at server.',
