efb5b659ccde2c4270c55e69029fbf2391f8389d
alexis <asanders>
2020-03-01 13:41:09 -0500

    testing location tracking on /eat-list and /sign-in from slides

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
+@app.route('/sign-in')
+def signin():
+    return render_template('/sign-in.html')
+	
 @app.route('/')
 def root():
     return render_template('/index.html')
@@ -24,5 +28,10 @@ def host():
 def attend():
     return render_template('/attend.html')
 
+@app.route('/eat-list')
+def eatlist():
+    return render_template('/eat-list.html')
+
+	
 if __name__ == '__main__':
         app.run(host='127.0.0.1', port=8080, debug=True) # updated port, so that when it runs locally, it runs on 8080
