ca12222d13e6e6061afeea1a6fcf81d57d8ad873
Alexis Sanders <asanders@merkleinc.com>
2020-03-04 18:55:50 -0500

    added show_page - a utility function that will help us expand as we do more with data

--- main.py
+++ main.py
@@ -17,25 +17,37 @@ def log(msg):
 @app.route('/eat')
 @app.route('/index.html')
 def root():
-    return render_template('/index.html',page_title='Food with Friends: Eat up!',h1='Eat Leftovers')
+    file = '/index.html'
+    title = 'Food with Friends: Eat up!'
+    h1 = 'Eat Leftovers'
+    return show_page(file,title,h1)
 	
 @app.route('/cook')
 def cook():
-    return render_template('/cook.html',page_title='Cook for Friends',h1='Cook')
+    file = '/cook.html'
+    title = 'Cook for Friends'
+    h1 = 'Cook'
+    return show_page(file,title,h1)
 	
 @app.route('/host')
 def host():
-    return render_template('/host.html',page_title='Host a Dinner & Make Friends',h1="Host")
+    file = '/host.html'
+    title = 'Host a Dinner & Make Friends'
+    h1 = "Host"
+    return show_page(file,title,h1)
 	
 @app.route('/attend')
 def attend():
-    return render_template('/attend.html',page_title='Attend a Dinner & Make Friends',h1="Attend")
+    file = '/attend.html'
+    title = 'Attend a Dinner & Make Friends'
+    h1 ="Attend"
+    return show_page(file,title,h1)
 
-@app.route('/eat-list')
+@app.route('/eat-list') # currently has location code from James
 def eatlist():
-    return render_template('/eat-list.html') # currently has location code from James
+    return render_template('/eat-list.html') 
 
-@app.route('/test')
+@app.route('/test') # currently testing templates
 def test():
     return render_template('test.html', page_title='Testing Templates!!!-uyfuar',h1="blah")
 
@@ -45,7 +57,16 @@ def authtoken():
     log('Token: ' + request.form.get('token'))
     d = { 'message': 'Auth Token received at server.' }
     return Response(json.dumps(d), mimetype='application/json')
-	
+
+# utility function that allows us to 
+# consolidate on the render_template function
+# will allow us to expand on parameters, as week04/gae/project2/main
+# start to get user, location, food, and dinner data
+def show_page(page, title, h1,errors=None):
+	return render_template(page,
+						   page_title=title, #on-page = parameter
+						   h1=h1,
+						   errors=errors)
 
 if __name__ == '__main__':
         app.run(host='127.0.0.1', port=8082, debug=True) # updated port, so that when it runs locally, it runs on 8030
