ec39822f6a81098d2cd7de95cba196d08936ffe9
Alex <50931199+als429@users.noreply.github.com>
2020-03-29 18:29:43 -0400

    adding logs tbd... if this works

--- forms.py
+++ forms.py
@@ -1,8 +1,17 @@
 from flask_wtf import FlaskForm
+log('flask_wtf imported')
 from wtforms import StringField, FloatField, FileField, SubmitField, DateTimeField, IntegerField
+log('wtforms imported')
 from wtforms.validators import DataRequired, InputRequired, NumberRange, Optional
+log('wtforms.validators imported')
 from wtforms.fields.html5 import DateTimeLocalField
+log('wtforms.fields.html5 imported')
 
+def log(msg):
+    """Log a simple message."""
+    # Look at: https://console.cloud.google.com/logs to see your logs.
+    # Make sure you have "stdout" selected.
+    print('main: %s' % msg)
 
 class MyFloatField(FloatField):
     def __init__(self, *args, **kwargs):
