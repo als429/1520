be5eaafbf6f5fd4adc650f035613e5f963ee3219
Alex <50931199+als429@users.noreply.github.com>
2020-04-13 17:38:03 -0400

    adding in stuff for form validation (since test won't work otherwise... :(

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google.auth.transport import requests # for backend sign in functionality
 
 import f_data # includes our data classes: User, Dinner, Food, Location
 import f_datastore
-from forms import FoodRegistrationForm, DinnerRegistrationForm
+from forms import FoodRegistrationForm, DinnerRegistrationForm, UploadForm
 from flask_wtf.csrf import CSRFProtect
 import os
 
@@ -180,7 +180,7 @@ def test_two():
 @app.route('/upload', methods=['GET','POST'])
 def test_three():
     log('test3 loaded')
-    form = RegisterForm()
+    form = UploadForm()
     if request.method == 'POST' and form.validate():
         log('in post for test3')
         uploaded_file = flask.request.files.get('file')
@@ -201,7 +201,7 @@ def test_three():
         log('uploaded from string')
         url = blob.public_url
         log('got url')
-        return url
+        return 'OK'
     return show_page('/upload.html','Image Upload', 'Image Upload')
 
 @app.route('/test-post', methods=['POST'])
